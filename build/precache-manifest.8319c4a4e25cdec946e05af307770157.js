cba4bd634e8e2892d9d746f02a190f9b8b8071a5
Mkhgkk <emmachalz@icloud.com>
2020-08-02 17:34:51 +0800

    added build to the repo

---
+++ build/precache-manifest.8319c4a4e25cdec946e05af307770157.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e593815f93f4e8d02f713c5f072b93bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798574ff3961280a8488",
+    "url": "/static/css/main.a510a319.chunk.css"
+  },
+  {
+    "revision": "d550c220a1358593feff",
+    "url": "/static/js/2.3537e2d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3537e2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798574ff3961280a8488",
+    "url": "/static/js/main.b2c89a5d.chunk.js"
+  },
+  {
+    "revision": "9a47c0c9d50de450c27b",
+    "url": "/static/js/runtime-main.6d410741.js"
+  },
+  {
+    "revision": "8f56a0ecb8a65ca755bcde98f38d91f3",
+    "url": "/static/media/loginBack.8f56a0ec.jpeg"
+  },
+  {
+    "revision": "cc9bf2868a34453dbb0368d18a2bac31",
+    "url": "/static/media/registerBack.cc9bf286.jpeg"
+  }
+]);
